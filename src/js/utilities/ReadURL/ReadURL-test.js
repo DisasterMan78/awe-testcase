7effe18c3366caefc980cb3375c6f33142496547
David Benson <dmebenson1978@gmail.com>
2018-01-10 19:57:53 +0000

    working xhr test using sinon.useFakeXMLHttpRequest

--- src/js/utilities/ReadURL/ReadURL-test.js
+++ src/js/utilities/ReadURL/ReadURL-test.js
@@ -2,168 +2,64 @@ import chai, { expect } from 'chai';
 import sinon, { spy } from 'sinon';
 
 import {ReadURL} from './ReadURL';
+import { __RewireAPI__ as readUrlRewire } from './ReadURL';
 
-
-
-/*
-*
-* Sinon useFakeXMLHttpRequest
-*
-*/
 describe('ReadURL', () => {
 
-  beforeEach(function() {
-
+  beforeEach(() => {
     this.xhr = sinon.useFakeXMLHttpRequest();
 
-    var requests = [];
+    this.requests = [];
+
     this.xhr.onCreate = (xhr) => {
-console.log('xhr created')
-      requests.push(xhr);
-    };
+      this.requests.push(xhr);
+    }
 
     this.response = 'Response not set';
   });
 
-  afterEach(function() {
+  afterEach(() => {
+    this.requests = [];
+
+    // global.XMLHttpRequest = this.realXMLHttpRequest;
     this.xhr.restore();
 
     this.response = 'Response not set';
   });
 
-  it('should get file content from an xhr request', () => {
+  it('should get file content from an xhr request', (done) => {
+
     const readURL = new ReadURL(),
-          url = 'http://dummy.com/file.js',
-          urlContent = `<awe.DisplayCode
-            htmlSelector={'.awe-login'}
-            jsxFile={'/src/js/components/AncoaAwe.js'}
-            jsxTag={'awe.Login'}
-            componentFile={'/src/js/components/Login/Login.js'}
-          />`;
-    let responseContent;
+          url = 'http://dummy.com/file.js';
 
-    readURL.getContent(url).then((type, response) =>{
-console.log('ReadURL-test response', response)
-      expect(type).to.equal('success');
-      expect(response).to.equal(urlContent);
-      done();
-    });
+    this.response = {
+      status: 200,
+      contentType: 'application/json',
+      content: `<awe.DisplayCode
+        htmlSelector={'.awe-login'}
+        jsxFile={'/src/js/components/AncoaAwe.js'}
+        jsxTag={'awe.Login'}
+        componentFile={'/src/js/components/Login/Login.js'}
+      />`
+    };
 
+    readURL.getContent(url)
+      .then((response) => {
+        expect(response).to.equal(this.response.content);
+        done();
+      })
+      .catch((error) => {
+console.log(error);
+        done();
+      });
 
-    window.setTimeout(() => {
-console.log('ReadURL-test trigger response')
-      this.requests[0].respond(200,
-        {
-          'Content-Type': 'application/json'
-        },
-        urlContent
-      )
-    }, 0);
+        this.requests[0].respond(this.response.status,
+          {
+            'Content-Type': this.response.contentType
+          },
+          this.response.content
+        );
 
   });
 
 });
-
-/*
-*
-* Sinon sandbox
-*
-*/
-// describe('ReadURL', () => {
-
-//   beforeEach(function() {
-
-//   this.sandbox = sinon.sandbox.create();
-
-//   this.sandbox.useFakeServer();
-
-//   this.server = this.sandbox.server;
-//   this.urlContent = `<awe.DisplayCode
-//           htmlSelector={'.awe-login'}
-//           jsxFile={'/src/js/components/AncoaAwe.js'}
-//           jsxTag={'awe.Login'}
-//           componentFile={'/src/js/components/Login/Login.js'}
-//         />`;
-
-//   this.server.respondWith([200, { "Content-Type": "application/text" }, this.urlContent]);
-
-//   });
-
-//   afterEach(function() {
-//     this.sandbox.restore();
-//   });
-
-//   it('should get file content from an xhr request', () => {
-//     const readURL = new ReadURL(),
-//           url = 'http://dummy.com/file.js';
-
-//     let responseContent;
-
-//     readURL.getContent(url).then((response) => {
-//       responseContent = response;
-//     });
-
-//     window.setTimeout(() => {
-// console.log('setTimeout')
-//       this.server.respond();
-//     }, 0);
-
-//     expect(responseContent).to.equal(this.urlContent);
-
-//   });
-
-// });
-
-/*
-*
-* XMLHttpRequest style with callbacks
-*
-*/
-// describe('ReadURL', () => {
-
-//   beforeEach(function() {
-
-//     this.xhr = sinon.useFakeXMLHttpRequest();
-
-//     var requests = [];
-//     this.xhr.onCreate = (xhr) => {
-// console.log('xhr created')
-//       requests.push(xhr);
-//     };
-
-//     this.response = 'Response not set';
-//   });
-
-//   afterEach(function() {
-//     this.xhr.restore();
-
-//     this.response = 'Response not set';
-//   });
-//   it('should get file content from an xhr request', () => {
-//     const readURL = new ReadURL(),
-//           url = 'http://dummy.com/file.js',
-//           urlContent = `<awe.DisplayCode
-//             htmlSelector={'.awe-login'}
-//             jsxFile={'/src/js/components/AncoaAwe.js'}
-//             jsxTag={'awe.Login'}
-//             componentFile={'/src/js/components/Login/Login.js'}
-//           />`,
-//           callback = sinon.spy();
-//     let responseContent;
-
-//     readURL.getContent(url, callback);
-
-//     expect(this.requests.length).to.equal(1);
-
-//     this.requests[0].respond(200,
-//       {
-//         'Content-Type': 'application/json'
-//       },
-//       urlContent
-//     );
-
-//     callback.calledWith('success', urlContent);
-
-//   });
-
-// });
--- src/js/utilities/ReadURL/ReadURL.js
+++ src/js/utilities/ReadURL/ReadURL.js
@@ -1,73 +1,25 @@
 'use strict';
 
-import * as axios from 'axios';
-
 export class ReadURL {
-  /*
-  *
-  * axios in promise
-  *
-  */
+
   getContent = (url) => {
+      let xhr = new global.XMLHttpRequest();
+      return new Promise((resolve, reject) => {
 
-console.log('ReadURL.getContent')
-    return new Promise((resolve, reject) => {
-console.log('ReadURL.getContent => Promise')
-      axios.get(url)
-        .then((response) => {
-console.log('ReadURL.getContent axios.get().then =>', response.data)
-          resolve(response.data);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
-  }
+          xhr.open("GET", url, false);
+          xhr.onreadystatechange = () => {
+              if (xhr.readyState === 4) {
+                  if (xhr.status === 200 || xhr.status == 0) {
+                      var allText = xhr.responseText;
+                      resolve(allText);
+                  } else {
+                      reject('ajax error:' + xhr.status + ' ' + xhr.responseText);
+                  }
+              }
+          };
+          xhr.send(null);
+      });
 
-  /*
-  *
-  * axios with callbacks
-  *
-  */
-//   getContent = (url, callback) => {
-// console.log('promise')
-//     axios.get(url)
-//       .then((response) => {
-// console.log('ReadURL.getContent axios.get().then =>', response.data)
-//         callback('success', response.data);
-//       })
-//       .catch((error) => {
-//         callback('error', error);
-//       });
-//   }
-
-
-  /*
-  *
-  * XMLHttpRequest style with callbacks
-  *
-  */
-//  getContent = (url, callback) => {
-// console.log('Promise')
-//       let xhr = new XMLHttpRequest();
-
-//       xhr.open("GET", url, false);
-//       xhr.onreadystatechange = () => {
-// console.log('onreadystatechange', xhr.readyState)
-//         if (xhr.readyState === 4) {
-//           if (xhr.status === 200) {
-// console.log('200')
-//             var allText = xhr.responseText;
-// console.log(allText)
-//             callback('success', allText);
-//           } else {
-// console.log('unable to open file', xhr.status, xhr.responseText)
-//             reject('error', xhr.status + ' ' + xhr.responseText);
-//           }
-//         }
-//       };
-//       xhr.send(null);
-
-//   };
+  };
 
 }
